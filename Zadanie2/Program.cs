0c71cd40f60ae629f5f386900dd7e0db5fe6a17b
Jakub Tanski <jakub.tanski@yoc.com>
2025-03-16 15:33:11 +0100

    added getAverage method

--- Zadanie2/Program.cs
+++ Zadanie2/Program.cs
@@ -1,5 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello, World!");
 Console.WriteLine("Modyfikacja 1!");
 Console.WriteLine("Modyfikacja 2!");
 Console.WriteLine("Modyfikacja 3!");
+
+static double getAverage (int[] array)
+{
+    double result = 0;
+    
+    foreach (int i in array)
+    {
+        result += i;
+    }
+    
+    return result / array.Length;
+} 
